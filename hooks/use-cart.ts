1ea3246b6d1eed3cbe0563fae3538656f61aabb6
garcia718gt4 <58898368+garcia718gt4@users.noreply.github.com>
2024-01-07 20:28:10 -0800

    typo error

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -22,7 +22,7 @@ const useCart = create(
         items: [], 
         addToCart: (data: Product) => {
             const currentItems = get().items; 
-            const existingItem = currentItems.find((item) => item.id === data.id); // 
+            const existingItem = currentItems.find((item) => item.id === data.id);
 
             if(existingItem) {
                 return toast("Item already in cart.");
