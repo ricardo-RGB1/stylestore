cf4fb8b1b75dd9e5aef2d390a707d5723cda89cc
garcia718gt4 <58898368+garcia718gt4@users.noreply.github.com>
2024-01-08 18:42:16 -0800

    update title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-           <p className='text-xl font-bold'>Store</p>
+           <p className='text-xl font-bold'>Style</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
